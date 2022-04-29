c0cae9b42462e6cbe3baf98a5b85d5fd703fe7ac
ninatokbaeva <tokbaevanina@gmail.com>
2022-04-28 22:18:28 -0400

    added login scenario 1

--- git_exercise/login_test.py
+++ git_exercise/login_test.py
@@ -3,3 +3,5 @@ print("Started working on login scenarios")
 print("started working on scenario 1")
 print("completed scenario 1")
 print("started working on scenario 2")
+
+print("some changes from login scenario")
